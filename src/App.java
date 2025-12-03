df0b33829a4f4186af5f890afe28c880612ac99e
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-12-03 15:39:56 -0300

    feat: add client class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Client Class
+        Client client1 = new Client("Vicente", "12345678900", "123 Main St", "555-1234", "vicente@example.com");
+        ArrayList<Client> clients = new ArrayList<>();
+        client1.registerCliente(client1, clients);
+
+        
+        client1.consultClient("12345678900", clients);
     }
 }
---
+++ src/Client.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Client{
+    String name;
+    String cpf;
+    String adress;
+    String phone;
+    String email;
+
+    public Client (String name, String cpf, String adress, String phone, String email){
+        this.name = name;
+        this.cpf = cpf;
+        this.adress = adress;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public void registerCliente(Client c, ArrayList<Client> clients){
+        try{
+            clients.add(c);
+            System.out.println("Cliente registrado com sucesso!!!");
+        }catch(Exception e){
+            System.out.println("Erro ao registrar cliente: " + e.getMessage());
+        }
+
+    }
+
+    public Client consultClient(String cpf, ArrayList<Client> clients){
+        for(Client c : clients){
+            if(c.cpf.equals(cpf)){
+                System.out.println("Cliente encontrado: " + c.namegi);
+                return c;
+            }
+        }
+        System.out.println("Cliente não encontrado.");
+        return null;
+    }
+
+
+
+}
